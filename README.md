50a1af20ca68e0196073b25ca772555a0acf2d86
Mihai <48624094+mihaichris@users.noreply.github.com>
2022-08-28 13:56:30 +0300

    add README file

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-This is a custom Solr Function Query
+# solr-function-query
+
+
+## Description
+
+Two custom Solr functions that can extract values from an array/object based on an argument given.
+
+## Installation
+
+First, you must package the project into a jar.
+
+```sh
+mvn package
+```
+
+And then add the jar file to the location where Solr is installed, into `dist/` directory.
+
+## Basic Usage
+
+```
+{
+    "id":"1",
+    "dictField":"{\"key_1\":\"val_1\", \"key_2\": \"val_2\"}",
+    "listField":"['val_1', 'val_2']"
+}
+```
+Having `valdict(dictField,"key_2")` and `vallist(listField,0)` in the `fl` (Field List) parameter would generate a response like:
+```
+{
+  "response":{
+    "docs":[
+      {
+        "id":"1",
+        "valdict(dictField,"key_2")":"val_2",
+        "vallist(listField,0)": "val_1"
+      }
+    ]
+  }
+} 
+```
+
+
+
--- src/test/java/mihaichris/solr/BaseSolrTestCase4J.java
+++ src/test/java/mihaichris/solr/BaseSolrTestCase4J.java
@@ -24,35 +24,17 @@ public abstract class BaseSolrTestCase4J extends SolrTestCaseJ4 {
                 SolrInputDocument doc1 = new SolrInputDocument();
                 doc1.addField("id", "1");
                 doc1.addField("product_id", "1");
-                List<String> tags_category_traffic1 = Arrays.asList("set cadou", "aparat ras", "aparate ras", "ras",
-                                "set cadou barbati", "aparat barbierit", "set barbierit", "aparat", "gilet", "gillet",
-                                "lama ras",
-                                "set barbati", "spuma ras", "barbierit", "big", "bricheta", "lame ras", "portofel",
-                                "rezerve aparat ras");
-                doc1.addField("tags_category_traffic", tags_category_traffic1);
                 assertNull(h.validateUpdate(adoc(doc1)));
 
                 SolrInputDocument doc2 = new SolrInputDocument();
                 doc2.addField("id", "2");
                 doc2.addField("product_id", "2");
-                List<String> tags_category_traffic2 = Arrays.asList("set cadou", "aparat ras", "aparate ras", "ras",
-                                "set cadou barbati", "aparat barbierit", "set barbierit", "aparat", "gilet", "gillet",
-                                "lama ras",
-                                "set barbati", "spuma ras", "barbierit", "big", "bricheta", "lame ras", "portofel",
-                                "rezerve aparat ras");
-                doc2.addField("tags_category_traffic", tags_category_traffic2);
                 assertNull(h.validateUpdate(adoc(doc2)));
 
                 SolrInputDocument doc3 = new SolrInputDocument();
                 doc3.addField("id", "3");
                 doc3.addField("product_id", "3");
-                List<String> tags_category_traffic3 = Arrays.asList("lame ras", "aparat ras", "aparat ras clasic",
-                                "barbierit",
-                                "razor", "safety razor", "aparat barbierit", "muhle", "muhle r89", "pamatuf", "ras",
-                                "set cadou barbati", "aparat barbierit", "set barbierit", "aparat", "gilet", "gillet");
-                doc3.addField("tags_category_traffic", tags_category_traffic3);
                 assertNull(h.validateUpdate(adoc(doc3)));
-                
                 assertNull(h.validateUpdate(commit()));
         }
 }
